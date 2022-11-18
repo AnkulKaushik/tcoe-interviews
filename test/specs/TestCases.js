d1f57d96a1ad45fc77790b11ed029be25b9bae33
Ankul <ankulkaushik12@gmail.com>
2022-11-18 19:45:02 +0530

    assertion implemented

--- test/specs/TestCases.js
+++ test/specs/TestCases.js
@@ -1,3 +1,6 @@
+const assert=require('assert')
+
+
 describe('validate test cases', () => {
 
     // Site Search Functionality 
@@ -6,20 +9,27 @@ describe('validate test cases', () => {
         browser.url("https://www.cnn.com/")
 
         const search = await $(".sc-bdVaJa");
+        // to check whether button is clickable
 
+        await expect(search).toBeClickable()
         await search.click()
      
         const click = await $("#header-search-bar");
 
-        await click.setValue("modi")
+        await click.setValue("Narendra Modi")
+        // to check input text contain Narendra Modi in it.
+        await expect(click).toHaveValue('Narendra Modi', { ignoreCase: true })
         await browser.executeAsync((done) => {
-                console.log('Wake me up before you go!');
                 setTimeout(done, 2000);
             });
         const btn = await $(".JwELA");
-        btn.click();
-        
-        expect(browser).toHaveUrlContaining('modi')
+        // to check whether button is clickable
+
+        await expect(btn).toBeClickable()
+
+        await btn.click();
+        // to check whether url have Modi word in it.
+        await expect(browser).toHaveUrlContaining('Modi')
 
     });
 
@@ -27,7 +37,12 @@ describe('validate test cases', () => {
 
    it ('verify video ',async()=>{
         browser.url("https://edition.cnn.com/videos/entertainment/2022/11/11/henry-winkler-career-struggle-grease-wtcw-cprog-vpx.cnn");
-       const video= await $(".video-inline__video-resource");
+        await expect(browser).toHaveUrlContaining('edition')
+        
+        const video= await $(".video-inline__video-resource");
+        // to check whether button is clickable
+
+       await expect(video).toBeClickable()
        await video.click()
        browser.pause(3000)
        await browser.executeAsync((done) => {
@@ -39,7 +54,14 @@ describe('validate test cases', () => {
     })
     it('verify related suggestion video', async() => {
         browser.url("https://edition.cnn.com/videos/business/2022/11/16/taylor-swift-concert-tour-presale-tickets-ticketmaster-cprog-cnntm-vpx.cnn");
+        // to check whether url have edition word in it.
+       
+        await expect(browser).toHaveUrlContaining('edition')
+
         const video= await $(".video-inline__video-resource");
+        // to check whether button is clickable
+
+       await expect(video).toBeClickable()
         await video.click()
         await browser.executeAsync((done) => {
             setTimeout(done, 15000);
@@ -57,11 +79,16 @@ describe('validate test cases', () => {
         let i = 1;
         await browser.setWindowRect(0, 0, 1200, 800);
         await browser.url("http://edition.cnn.com/travel/gallery/top-christmas-markets/index.html")
+        await expect(browser).toHaveUrlContaining('edition')
        
         const elem = await $('.Image__component');
         await elem.scrollIntoView();
         const next_image = await $(".GalleryHeroDecorators__next");
         do {
+        // to check whether button is clickable
+
+       await expect(next_image).toBeClickable()
+
             await next_image.click()
             await browser.executeAsync((done) => {
                 setTimeout(done, 2000);
@@ -71,6 +98,10 @@ describe('validate test cases', () => {
 
         const prev_image = await $(".GalleryHeroDecorators__previous");
         do {
+        // to check whether button is clickable
+
+       await expect(prev_image).toBeClickable()
+
             await prev_image.click()
             await browser.executeAsync((done) => {
                 setTimeout(done, 2000);
